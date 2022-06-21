ee9d20c736528051d30a85523f5c055554dd2724
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2022-06-21 19:09:41 +0700

    Using ejs as a view engine

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ const app = express();
 const db = require("./database/db");
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
 app.use("/api", queues);
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
 app.listen(PORT, () => console.log(`App running on http://localhost:${PORT}`));
