4f9a5fa92e4c136b53622a71363a1be89978a86c
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2022-06-23 15:55:47 +0700

    Create db server

--- database/db.js
+++ database/db.js
@@ -4,4 +4,16 @@ const FileSync = require("lowdb/adapters/FileSync");
 const adapter = new FileSync("./database/db.json");
 const db = low(adapter);
 
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./database/Students.json");
+const middlewares = jsonServer.defaults();
+const PORT = 3000;
+
+server.use(middlewares);
+server.use(router);
+server.listen(PORT, () => {
+  console.log("JSON server is running");
+});
+
 module.exports = db;
