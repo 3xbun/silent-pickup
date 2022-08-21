0b55cd3bb40c55441b690ba0cafecbc3994d2124
Bunnasorn Kaewsiri <bunnasorn.k@ku.th>
2022-08-21 20:26:49 +0700

    implement mongoose

--- routes/Queues.js
+++ routes/Queues.js
@@ -1,46 +1,43 @@
 const express = require("express");
 const dayjs = require("dayjs");
-const { db } = require("../database/db");
+const { Queues } = require("../database/db");
 
 const router = express.Router();
 
 // Get queues
-router.get("/", (req, res) => {
-  const queues = db.get("queues").value();
+router.get("/", async (req, res) => {
+  const queues = await Queues.find();
   res.json(queues);
 });
 
 // Get queue by ID
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   const id = req.params.id;
-  const queue = db.get("queues").find({ id: id }).value();
+  const queue = await Queues.findOne({ id: id });
 
   res.json(queue);
 });
 
 // Add queue
-router.post("/add/queue", (req, res) => {
-  const queue = {
-    time: dayjs(),
+router.post("/add/queue", async (req, res) => {
+  const queue = new Queues({
     id: String(req.body.id),
     name: req.body.name,
     class: req.body.class,
     pickupType: req.body.pickupType,
-  };
+  });
 
-  db.get("queues").push(queue).last().write();
+  await queue.save();
 
-  res.json(queue);
+  return res.status(201).json(queue);
 });
 
 // Delete queue
-router.delete("/delete/:id", (req, res) => {
+router.delete("/delete/:id", async (req, res) => {
   const id = req.params.id;
-  const queueID = db.get("queues").find({ id: id }).value();
+  await Queues.findOneAndDelete({ id: id });
 
-  db.get("queues").remove({ id: id }).write();
-
-  res.json(queueID);
+  res.status(200);
 });
 
 module.exports = router;
