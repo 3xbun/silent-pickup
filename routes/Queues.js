8330556a734609da2c1ef480f62551f5826fd4ac
Bunnasorn Kaewsiri <bunnasorn.k@outlook.com>
2023-03-20 14:18:57 +0700

    Better handling request

--- routes/Queues.js
+++ routes/Queues.js
@@ -12,15 +12,17 @@ router.get("/", async (req, res) => {
 // Get queue by classroom
 router.get("/class/:class", async (req, res) => {
   const prefix = {
-    "k": "อ",
-    "p": "ป"
-  }
+    k: "อ",
+    p: "ป",
+  };
 
-  const cls = (req.params.class).split("");
+  const cls = req.params.class.split("");
 
-  const queues = await Queues.find({ class: { $regex: `^${prefix[cls[0]]}.${cls[1]}` } })
-  res.status(201).json(queues)
-})
+  const queues = await Queues.find({
+    class: { $regex: `^${prefix[cls[0]]}.${cls[1]}` },
+  });
+  res.status(201).json(queues);
+});
 
 // Get queue by ID
 router.get("/:id", async (req, res) => {
@@ -33,7 +35,7 @@ router.get("/:id", async (req, res) => {
 // Add queue
 router.post("/add/queue", async (req, res) => {
   if (await Queues.findOne({ id: String(req.body.id) })) {
-    return res.status(400)
+    return res.status(400);
   } else {
     const queue = new Queues({
       id: String(req.body.id),
@@ -41,10 +43,10 @@ router.post("/add/queue", async (req, res) => {
       class: req.body.class,
       pickupType: req.body.pickupType,
     });
-  
+
     await queue.save();
-  
-    return res.status(201).json(queue);  
+
+    return res.status(201).json(queue);
   }
 });
 
@@ -53,12 +55,12 @@ router.delete("/delete/:id", async (req, res) => {
   const id = req.params.id;
   await Queues.findOneAndDelete({ id: id });
 
-  res.status(200);
+  res.status(200).json(Queues);
 });
 
 // Delete all queues
 router.delete("/delete", async (req, res) => {
-  await Queues.deleteMany()
-})
+  await Queues.deleteMany();
+});
 
 module.exports = router;
